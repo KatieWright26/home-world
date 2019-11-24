874ffc5560e05c72e5aa4fc7ac18dcb903e7377e
Katie Wright <katie.wrght@gmail.com>
2019-11-24 23:32:19 +0100

    import player sprint and enable movement around map

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ const config = {
     preload: preload,
     create: create,
     update: update
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 } // Top down game, so no gravity
+    }
   }
 };
 
 const game = new Phaser.Game(config);
+let player;
+let showDebug = false;
+let cursors;
 
 function preload(){
   this.load.image("tiles", "/assets/tilesets/tilemap.png");
   this.load.tilemapTiledJSON("map", '/assets/tilemaps/map.json');
-  console.log(this)
+  this.load.image("atlas", "/assets/atlas/atlas.png");
 };
 
 function create() {
@@ -24,7 +33,47 @@ function create() {
   const tileset = map.addTilesetImage("New tileset", "tiles");
   const belowLayer = map.createStaticLayer("below player", tileset, 0, 0);
   const world = map.createStaticLayer("world", tileset, 0, 0);
+
+  world.setCollisionByProperty({ collides: true });
   // const aboveLayer = map.createStaticLayer("above player", tileset, 0, 0);
+  const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+  player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "atlas")
+
+  this.physics.add.collider(player, world);
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  cursors = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left:  cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5,
+  });
 };
 
-function update(time, delta){};
+function update(time, delta){
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  player.body.setVelocity(0);
+
+  if(cursors.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(speed);
+  }
+
+  if(cursors.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(speed);
+  }
+
+  player.body.velocity.normalize().scale(speed);
+  controls.update(delta)
+};
