872daf0b5f9507e3a64df9e0cfe363df1856072c
Katie Wright <katie.wrght@gmail.com>
2019-11-26 17:33:57 +0100

    create dynamic layours for door tiles and set up callback to render new scene

--- js/activeTiles/worldSceneDoors.js
+++ js/activeTiles/worldSceneDoors.js
@@ -1,12 +1,46 @@
-import Phaser from 'phaser';
+import { createDetectDoorCallback } from '../newScene';
 
-function worldSceneDoors(world, cb, scene) {
-  world.setTileLocationCallback(15, 7, 1, 1, cb, scene);
-  world.setTileLocationCallback(30, 7, 1, 1, cb, scene);
-  world.setTileLocationCallback(36, 18, 1, 1, cb, scene);
-  world.setTileLocationCallback(15, 7, 1, 1, cb, scene);
-  world.setTileLocationCallback(16, 18, 1, 1, cb, scene);
-  world.setTileLocationCallback(31, 40, 1, 1, cb, scene);
+function worldSceneDoors(world, scene) {
+  world.setTileLocationCallback(
+    15,
+    7,
+    1,
+    1,
+    createDetectDoorCallback(scene, 'HomeScene'),
+    scene
+  );
+  world.setTileLocationCallback(
+    30,
+    7,
+    1,
+    1,
+    createDetectDoorCallback(scene, 'NeighbourScene'),
+    scene
+  );
+  world.setTileLocationCallback(
+    36,
+    18,
+    1,
+    1,
+    createDetectDoorCallback(scene, 'NeighbourScene'),
+    scene
+  );
+  world.setTileLocationCallback(
+    16,
+    18,
+    1,
+    1,
+    createDetectDoorCallback(scene, 'NeighbourScene'),
+    scene
+  );
+  world.setTileLocationCallback(
+    27,
+    19,
+    2,
+    1,
+    createDetectDoorCallback(scene, 'MarktScene'),
+    scene
+  );
 }
 
 export { worldSceneDoors };
