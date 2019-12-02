8899ac347231ba5ec80f1ed4b08310209f3a4bdc
Katie Wright <katie.wrght@gmail.com>
2019-12-02 22:00:45 +0100

    Updates

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import NeighbourScene from './js/scenes/neighbourScene';
 
 const config = {
   type: Phaser.AUTO,
-  width: 400,
-  height: 300,
-  zoom: 2,
+  width: 350,
+  height: 250,
+  zoom: 2.5,
   parent: 'game-container',
   pixelArt: true,
   scene: [WorldScene, HomeScene, MarktScene, NeighbourScene],
